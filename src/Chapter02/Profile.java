68eca1664d5a88d17d6dd7995b8e968420c15dc1
eohyemin <eohyemin@gmail.com>
2021-06-15 21:39:39 +0900

    chapter 02

---
+++ src/Chapter02/Profile.java
@@ -0,0 +1,10 @@
+package Chapter02;
+
+public class Profile {
+    public static void main(String[] args) {
+        String name = "혜민";
+        int age = 27;
+        System.out.println("My name is "+name);
+        System.out.println("My age is "+age);
+    }
+}
