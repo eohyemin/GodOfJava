68ca50399bc7458c9e4937ee0de86ee416de2540
eohyemin <eohyemin@gmail.com>
2021-06-20 19:00:50 +0900

    Chapter 26

---
+++ src/Chapter26/io/practice/FileSizeSummary.java
@@ -0,0 +1,63 @@
+package Chapter26.io.practice;
+
+import java.io.File;
+import java.text.DecimalFormat;
+
+public class FileSizeSummary {
+    private final long KB = 1024;
+    private final long MB = 1024 * 1024;
+    private final long GB = 1024 * 1024 * 1024;
+    DecimalFormat formatter = null;
+
+    public static void main(String[] args) {
+        FileSizeSummary sizeSummary = new FileSizeSummary();
+        String path = "C:\\godofjava";
+        long sum = sizeSummary.printFileSize(path);
+        System.out.println(path+"'s total size = " +sum);
+    }
+
+    public FileSizeSummary() {
+        String pattern = "#,##0.0#";
+        formatter = new DecimalFormat(pattern);
+    }
+
+    public long printFileSize(String dirName) {
+        File dir = new File(dirName);
+        long sum = 0;
+        if(dir.isDirectory()) {
+            File[] files = dir.listFiles();
+            for(File file: files) {
+                if(file.isFile()) {
+                    String tempFileName = file.getAbsolutePath();
+                    long fileLength = file.length();
+                    System.out.println(tempFileName+ "=" +fileLength);
+                    sum += fileLength;
+                } else {
+                    String tempDirName = file.getAbsolutePath();
+                    long fileLength = printFileSize(tempDirName);
+                    printInfo(tempDirName, fileLength, true);
+                    sum += fileLength;
+                }
+            }
+        }
+        return sum;
+    }
+    public void printInfo(String path, long fileLength, boolean dirFlag) {
+        if(dirFlag) {
+            path = "[" + path + "] TOTAL";
+        }
+        System.out.println(path + "=" +convertFileLength(fileLength) + "("+fileLength+")");
+    }
+
+    private String convertFileLength(long fileLength) {
+        if(fileLength < KB) {
+            return fileLength + " b";
+        } else if(fileLength < MB) {
+            return formatter.format(1.0 * fileLength/KB) + " kb";
+        } else if(fileLength < GB) {
+            return formatter.format(1.0 * fileLength/MB) + " mb";
+        } else {
+            return formatter.format(1.0 * fileLength/GB) + " gb";
+        }
+    }
+}
