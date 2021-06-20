5f08aac39dc9f0dc4f02fe460746b034a674e2dd
eohyemin <eohyemin@gmail.com>
2021-06-20 18:29:04 +0900

    Chapter 25

---
+++ src/Chapter25/thread/practice/TimerMain.java
@@ -0,0 +1,8 @@
+package Chapter25.thread.practice;
+
+public class TimerMain {
+    public static void main(String[] args) {
+        TimerThread thread = new TimerThread();
+        thread.start();
+    }
+}
